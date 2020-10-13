012e7a72888f593c077da492802d1ae0154e78aa
Tesso Costa <tesso.costa@skipthedishes.ca>
2020-10-13 09:55:33 -0600

    Simple Spring web MVC Project

---
+++ src/main/java/br/eti/tmc/softarc/SoftarcApplication.java
@@ -0,0 +1,13 @@
+package br.eti.tmc.softarc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftarcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftarcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/eti/tmc/softarc/SoftarcApplicationTests.java
@@ -0,0 +1,13 @@
+package br.eti.tmc.softarc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftarcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
